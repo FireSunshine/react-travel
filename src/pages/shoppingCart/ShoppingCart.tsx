ff3fe1f9fd04035da1055f20e87e057d1efabee8
sunshine <2898205320@qq.com>
2022-11-23 23:23:12 +0800

    购物车页面初始化

--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { ProductList, PaymentCard } from '../../components';
+import { MainLayout } from '../../layouts/mainLayout';
+import { Row, Col, Affix } from 'antd';
+import { PageContent } from './shoppingCart.style';
 
 export const ShoppingCart = () => {
-  return <div>ShoppingCart</div>;
+  return (
+    <MainLayout>
+      <PageContent>
+        <Row>
+          <Col span={16}>
+            <div className="product-list-container">{/* <ProductList/> */}</div>
+          </Col>
+          <Col span={8}>
+            <Affix>
+              <div className="payment-card-container">{/* <PaymentCard /> */}</div>
+            </Affix>
+          </Col>
+        </Row>
+      </PageContent>
+    </MainLayout>
+  );
 };
---
+++ src/pages/shoppingCart/shoppingCart.style.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const PageContent = styled.div`
+  .product-list-container {
+    padding: 20px;
+    margin-top: 20px;
+    background: white;
+  }
+
+  .payment-card-container {
+    margin-top: 20px;
+    margin-left: 50px;
+  }
+`;
